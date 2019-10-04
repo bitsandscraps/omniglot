9024270eb8d6df8a7388f0da8b8807600bf38dca
bitsandscraps <17763502+bitsandscraps@users.noreply.github.com>
2019-10-04 17:14:03 +0900

    changed to SHA-256

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ curl -LO https://github.com/brendenlake/omniglot/raw/master/python/images_evalua
 unzip images_background.zip
 unzip images_evaluation.zip
 python to_numpy.py
-sha1sum images_background.npz > images_background.npz.sha1
-sha1sum images_evaluation.npz > images_evaluation.npz.sha1
+sha256sum images_background.npz > images_background.npz.sha256
+sha256sum images_evaluation.npz > images_evaluation.npz.sha256
