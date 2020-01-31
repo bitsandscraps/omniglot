b0481240b7eb42393285a93ce7d2f266ff10c62b
bitsandscraps <17763502+bitsandscraps@users.noreply.github.com>
2020-01-31 17:17:34 +0900

    updated requirements.txt

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-curl -LO https://github.com/brendenlake/omniglot/raw/master/python/images_background.zip
-curl -LO https://github.com/brendenlake/omniglot/raw/master/python/images_evaluation.zip
-unzip images_background.zip
-unzip images_evaluation.zip
-python to_numpy.py
-sha256sum images_background.npz > images_background.npz.sha256
+rm -rf images_background images_evaluation && \
+curl -LO https://github.com/brendenlake/omniglot/raw/master/python/images_background.zip && \
+curl -LO https://github.com/brendenlake/omniglot/raw/master/python/images_evaluation.zip && \
+unzip images_background.zip && \
+unzip images_evaluation.zip && \
+python to_numpy.py && \
+sha256sum images_background.npz > images_background.npz.sha256 && \
 sha256sum images_evaluation.npz > images_evaluation.npz.sha256
